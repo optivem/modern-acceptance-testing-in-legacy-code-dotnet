2c7a6e2d58ab0a97e161833ccb8c8eef673f76ab
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-17 14:48:18 +0100

    Refactor Core project structure and test config scripts
    Replaced Core.csproj with Core.Root.csproj as the main aggregator for Core subprojects and dependencies. Updated solution and project references accordingly. Set explicit RootNamespace for each Core subproject. Introduced a solution folder for Core projects. Enhanced system test scripts to support multiple test configurations and selection by test ID, improving modularity and flexibility.

--- Run-SystemTests.Config.ps1
+++ Run-SystemTests.Config.ps1
@@ -2,14 +2,22 @@
 # This file contains configuration values for Run-SystemTests.ps1
 
 $Config = @{
-    # Test Configuration
-    TestInstallCommands = @(
-        "pwsh bin/Debug/net8.0/playwright.ps1 install"
-    )
-    SmokeTestCommand = "dotnet test --filter 'FullyQualifiedName~SmokeTests' --logger 'trx;LogFileName=testResults.trx' --logger 'html;LogFileName=testResults.html' --logger 'console;verbosity=detailed'"
-    E2ETestCommand = "dotnet test --filter 'FullyQualifiedName~E2eTests' --logger 'trx;LogFileName=testResults.trx' --logger 'html;LogFileName=testResults.html' --logger 'console;verbosity=detailed'" 
 
-    TestReportPath = "TestResults\testResults.html"
+    Tests = @(
+        @{  Id = "smoke";
+            Name = "Smoke Tests";
+            Command = "dotnet test --filter 'FullyQualifiedName~SmokeTests' --logger 'trx;LogFileName=testResults.trx' --logger 'html;LogFileName=testResults.html' --logger 'console;verbosity=detailed'";
+            Path = "system-test";
+            TestReportPath = "TestResults\testResults.html"
+            TestInstallCommands = "pwsh bin/Debug/net8.0/playwright.ps1 install"; },
+        @{ 
+            Id = "e2e";
+            Name = "E2E Tests";
+            Command = "dotnet test --filter 'FullyQualifiedName~E2eTests' --logger 'trx;LogFileName=testResults.trx' --logger 'html;LogFileName=testResults.html' --logger 'console;verbosity=detailed'";
+            Path = "system-test";
+            TestReportPath = "TestResults\testResults.html";
+            TestInstallCommands = "pwsh bin/Debug/net8.0/playwright.ps1 install";  }
+    )
 }
 
 # Export the configuration
