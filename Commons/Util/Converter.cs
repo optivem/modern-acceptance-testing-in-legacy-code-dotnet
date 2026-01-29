b2bba629843c6f8d975560c45f8162d399e02fdc
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-29 15:27:09 +0100

    refactor: Rename conversion methods in Converter for clarity and streamline null handling

--- Commons/Util/Converter.cs
+++ Commons/Util/Converter.cs
@@ -2,27 +2,19 @@ namespace Commons.Util;
 
 public static class Converter
 {
-    public static decimal? ToDecimal(string? value)
+    public static decimal? ToBigDecimal(string? value)
     {
-        if (value == null)
-        {
-            return null;
-        }
-        return decimal.Parse(value);
+        return To(value, decimal.Parse);
     }
 
-    public static decimal ToDecimal(double value)
+    public static decimal ToBigDecimal(double value)
     {
         return (decimal)value;
     }
 
-    public static string? FromDecimal(decimal? value)
+    public static string? FromBigDecimal(decimal? value)
     {
-        if (value == null)
-        {
-            return null;
-        }
-        return value.Value.ToString();
+        return From(value, v => v.ToString());
     }
 
     public static string FromDouble(double value)
@@ -32,46 +24,38 @@ public static class Converter
 
     public static int? ToInteger(string? value)
     {
-        if (string.IsNullOrEmpty(value))
-        {
-            return null;
-        }
-        return int.Parse(value);
+        return To(value, int.Parse);
     }
 
     public static string? FromInteger(int? value)
     {
-        if (value == null)
-        {
-            return null;
-        }
-        return value.Value.ToString();
+        return From(value, v => v.ToString());
     }
 
     public static double? ToDouble(string? value)
     {
-        if (string.IsNullOrEmpty(value))
-        {
-            return null;
-        }
-        return double.Parse(value);
+        return To(value, double.Parse);
     }
 
     public static DateTime? ToInstant(string? value)
     {
-        if (string.IsNullOrEmpty(value))
-        {
-            return null;
-        }
-        return DateTime.Parse(value);
+        return To(value, DateTime.Parse);
     }
 
     public static string? FromInstant(DateTime? value)
     {
-        if (value == null)
-        {
-            return null;
-        }
-        return value.Value.ToString("O"); // ISO 8601 format
+        return From(value, v => v.ToString("O")); // ISO 8601 format
+    }
+
+    private static string? From<TSource>(TSource? value, Func<TSource, string> converter)
+        where TSource : struct
+    {
+        return value == null ? null : converter(value.Value);
+    }
+
+    private static TResult? To<TResult>(string? value, Func<string, TResult> converter)
+        where TResult : struct
+    {
+        return string.IsNullOrEmpty(value) ? null : converter(value);
     }
 }
